49fb86e9fc9ae2dd2703898189897f814c83a58f
Amr15066 <amr22126@outlook.com>
2025-12-25 20:56:33 +0000

    accounting for width and height

--- image_render.cpp
+++ image_render.cpp
@@ -10,10 +10,10 @@ int main() {
     for (int j = 0; j < width; j++) {
         for (int i = 0; i < height; i++) 
         {
-            int r = i;
-            int g = j;
-            int b = 0;
-            
+            int r = (double(i)/height) * 255;
+            int g = (double(j)/width) * 255;
+            int b = 0.0;
+
             std::cout << r << " " << g << " " << b << "\n";
         }
     }
